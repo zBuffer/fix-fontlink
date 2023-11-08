1146e01c841e946aefb38a0baeee2623ca3bacf4
Lance Chong <hliang@datasonic.com.my>
2023-11-08 20:30:30 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Fix_FontLink
+{
+    class RegistryValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            // Registry key path
+            string registryKeyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontLink\SystemLink";
+
+            // Define the custom sorting order
+            List<string> sortingOrder = (new string[] { "MSYH.TTC", "MSJH.TTC", "MEIRYO.TTC", "MALGUN.TTF", "TAHOMA.TTF" }).ToList();
+
+            using (FileStream fs = File.OpenWrite(String.Format("{0}-regbackup.reg", DateTime.Now.ToString("yyyyMMddHHmmss"))))
+            {
+                using (TextWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine("Windows Registry Editor Version 5.00");
+                    writer.WriteLine();
+                    writer.WriteLine("[{0}]", registryKeyPath);
+
+                    // Registry key to inspect
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKeyPath, true))
+                    {
+                        if (key == null)
+                        {
+                            Console.WriteLine("Registry key not found {0}", registryKeyPath);
+                            return;
+                        }
+
+                        // Get all the value names under the registry key
+                        string[] valueNames = key.GetValueNames().Where(p => p.StartsWith("Segoe UI")).ToArray();
+
+                        // Iterate through the value names and add them to the list
+                        List<RegistryValue> registryValues = new List<RegistryValue>();
+                        foreach (string valueName in valueNames)
+                        {
+                            // Create a list of RegistryValue objects to store key and value pairs
+                            List<string> values = (key.GetValue(valueName) as string[]).ToList();
+
+                            Console.WriteLine("Backing up {0}", valueName);
+
+                            // Backup registry value
+                            writer.WriteLine("\"{0}\"=hex(7):{1}", valueName, ToHex7(values));
+                            writer.Flush();
+
+                            // Custom sorting
+                            values.Sort((v1, v2) =>
+                            {
+                                int i1 = sortingOrder.FindIndex(p => v1.StartsWith(p));
+                                int i2 = sortingOrder.FindIndex(p => v2.StartsWith(p));
+                                if (i1 != -1 && i2 != -1)
+                                {
+                                    return i1.CompareTo(i2);
+                                }
+                                if (i2 != -1)
+                                {
+                                    return 1;
+                                }
+                                if (i1 != -1)
+                                {
+                                    return -1;
+                                }
+                                return 0;
+                            });
+
+                            Console.WriteLine("Applying changes to {0}", valueName);
+                            key.SetValue(valueName, values.ToArray());
+                        }
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey(true);
+        }
+
+        static string ToHex7(List<string> values)
+        {
+            UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+            values = new List<string>(values);
+            values.Add("");
+            return String.Join(",", values.Select(p =>
+            {
+                List<byte> arr = unicodeEncoding.GetBytes(p).ToList();
+                arr.Add(0);
+                arr.Add(0);
+
+                return String.Join(",", BitConverter.ToString(arr.ToArray()).Replace('-', ',').ToLower());
+            }));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Fix-FontLink
+
+This program is written to fix the display of Chinese font on Windows 10/Windows 11 English.
+
+## Problem
+
+When displaying Simplified Chinese characters, Windows Font Linking by default falls back to Japanese font (Meiryo), 
+then Traditional Chinese font (JhengHei), then Simplified Chinese font (YaHei).
+
+Because the above fonts do not have a consistent size and weight, the display of Simplified Chinese characters are ugly 
+when there is a different mix of fonts involved.
+
+This problem was not present when Microsoft had license for the Arial Unicode font.
+
+## Solution
+
+This program sorts the Font Linking priority, optimized for the display of Simplified Chinese characters in the following order:
+
+```
+"MSYH.TTC", "MSJH.TTC", "MEIRYO.TTC", "MALGUN.TTF", "TAHOMA.TTF"
+```
+
+In other words, the priority looks like:
+
+1. Simplified Chinese
+2. Traditional Chinese
+3. Japanese
+4. Korean
+5. Latin
+
+If you would like to optimize it to display other languages instead, change the array in `Program.cs:25` and put the font corresponding 
+to your language as the first element.
+
+i.e.
+```
+"MSJH.TTC", "MSYH.TTC", "MEIRYO.TTC", "MALGUN.TTF", "TAHOMA.TTF"
+```
+
+This would optimize the display of Traditional Chinese characters.
